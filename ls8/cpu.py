d858c976fbbe335a4f1961939b6d56fc0e1fad27
Restyled.io <commits@restyled.io>
2020-02-28 18:05:58 +0000

    Restyled by yapf

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -99,8 +99,7 @@ class CPU:
         """
 
         print(
-            f"TRACE: %02X | %02X %02X %02X |"
-            % (
+            f"TRACE: %02X | %02X %02X %02X |" % (
                 self.pc,
                 # self.fl,
                 # self.ie,
